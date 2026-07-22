f5975523cb9d9f1803a413a8c8bf717c44909dec
aanyagoyal28-crypto <aanyagoyal28@gmail.com>
2026-07-22 17:25:46 +0400

    Add implementation of variable types and print statements in S01_A01

---
+++ Session 01_The_Proper_Foundation/Assignment/S01_A01.py
@@ -0,0 +1,69 @@
+#q01
+print(type(42)) #int
+print(type(42.0))#float
+print(type("42"))#str
+#q02
+box = 100
+box = "sold out"
+print(box)  # it will change to the latest value of variable 
+print(type(box)) # after changing the value of varibale, type will be str instead of int
+#q03
+shop_name = "Prakash Mart"
+sugar_price = 42
+in_stock_flag = True 
+print (shop_name)
+print (sugar_price)
+print (in_stock_flag)
+#q04
+name = "Basmati Rice"
+price = 320
+weight = 4.5
+in_stock = True 
+discount = None 
+print(type(name))
+print(type(price))
+print(type(weight))
+print(type(in_stock))
+print(type(discount))
+#q05
+rice_price = 58
+print (rice_price) 
+rice_price = 62
+print (rice_price)
+shelf_1 = "Rice"
+shelf_2 = "Dal"
+shelf_1, shelf_2 = shelf_2, shelf_1
+print (shelf_1 , shelf_2)
+#q06
+# Purpose: Print the Praksh Mart banner.
+# Author: Aanya
+print("Welcome to", "Praksh Mart")
+#A short banner gives customers key information immediately.
+print("Open 7 AM to 10 PM")
+print("System ready")
+#q07
+shop_name = "Praksh Mart"
+#it did not run before because variables are case sensitive.
+print(shop_name)
+#q08
+name ="Sugar"
+price = 42
+weight = 1.0
+in_stock = True 
+discount= None
+print(name)
+print(price)
+print(weight)
+print(in_stock)
+print(discount)
+price= 45
+print(price) #variables can change their value 
+#q09
+shop_name = "Praksh Mart"
+year_opened = 1998
+open_now = True
+todays_special = None
+print("shop name:" , shop_name)
+print("year opened:" , year_opened)
+print("open now:" , open_now)
+print("today's special:" , todays_special)
