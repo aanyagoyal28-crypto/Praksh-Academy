ec0c6715891b3cc6bb4bb32c9de1996b9d94ef59
aanyagoyal28-crypto <aanyagoyal28@gmail.com>
2026-07-25 14:23:35 +0400

    Add initial implementation of S02_A01.py with basic arithmetic and user input examples

---
+++ Session 02_Numbers_Conversions_Talking_to_Users/Class/Assignment/S02_A01.py
@@ -0,0 +1,36 @@
+#q1
+int("185") + 15
+print (int("185")+15)
+
+#q2
+500 // 7
+print ("Number of whole eggs:" , 500 // 7)
+print ("change remaining:" , 500 % 7)
+
+#q3
+stock= 8
+print (stock<10)
+print (stock == 0)
+print (stock>= 5 and stock < 10)
+
+#q4
+bill = 420
+paid = 500 
+print (f" Change to return: {paid-bill}")
+
+#q5
+bill = int(input("Enter a bill amount:"))
+print (f"Rs 500 x {bill//500}")
+bill= bill % 500
+print (f"Rs 100 x {bill//100}")
+bill= bill % 100
+print (f"Rs 20 x {bill//20}")
+bill= bill % 20
+print (f"Rs 5 x {bill//5}")
+
+
+
+
+
+
+
